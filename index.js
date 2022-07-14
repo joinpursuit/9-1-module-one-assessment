58b143af1314fab1438585e2d1d8664bce5f0949
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-07-14 11:09:24 -0400

    findById

--- index.js
+++ index.js
@@ -117,7 +117,14 @@ function countByRating(movies) {
       // Toy Story 4
     };
  */
-function findById() { }
+function findById(movies, id) {
+  for (const movie of movies) {
+    if(movie[`imdbID`] === id) {
+      return movie;
+    }
+  }
+  return null;
+}
 
 /**
  * filterByGenre()
